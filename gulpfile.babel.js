4fa0f7a2777b64bf4f0a84f50df9757b3acf1db9
Luis Montero <luismb@luismb-thi-m.local>
2017-04-30 20:06:46 -0600

    Added gulp config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,23 +11,25 @@ import buffer from 'vinyl-buffer';
 import browserSync from 'browser-sync';
 import imagemin   from 'gulp-imagemin';
 
+const config = require("./gulp.config")();
+
 gulp.task('sass', ()=>{
-  return gulp.src('./app/sass/**/*.scss') 
+  return gulp.src(`${config.src}sass/**/*.scss`) 
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) 
         .pipe(autoprefixer())
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist/css')); 
+        .pipe(gulp.dest(`${config.dest}/css`)); 
 });
 
 gulp.task('html',()=>{
-	gulp.src("app/**/*html")
-	.pipe(gulp.dest("dist/"))
+	gulp.src(`${config.src}**/*html`)
+	.pipe(gulp.dest(`${config.dest}`))
 });
 
 gulp.task('js',()=>{
 	return browserify({
-		entries: './app/js/app.js'
+		entries: `${config.src}/js/app.js`
 	})
 	.bundle()
     .pipe(source('bundle.js'))
@@ -35,27 +37,27 @@ gulp.task('js',()=>{
     .pipe(uglify())
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist/js/'));
+    .pipe(gulp.dest(`${config.dest}js/`));
 
 });
 
 gulp.task('assets',()=>{
-	gulp.src("app/img/*")
+	gulp.src(`${config.src}img/*`)
 	.pipe(imagemin())
-	.pipe(gulp.dest("dist/img/"))
+	.pipe(gulp.dest(`${config.dest}img/`))
 });
 
 gulp.task('serve', ['sass','js','html','assets'], ()=>{
 
     browserSync.init({
-        server: "./dist"
+        server: config.dest
     });
 
-    gulp.watch("app/sass/*.scss", ['sass']);
-    gulp.watch("app/js/*.js", ['js']);
-    gulp.watch("app/**/*.html", ['html']);
-    gulp.watch("app/img/*", ['assets']);
-    gulp.watch(["dist/**/*"]).on('change', browserSync.reload);
+    gulp.watch(`${config.src}sass/*.scss`, ['sass']);
+    gulp.watch(`${config.src}js/*.js`, ['js']);
+    gulp.watch(`${config.src}**/*.html`, ['html']);
+    gulp.watch(`${config.src}img/*`, ['assets']);
+    gulp.watch([`${config.dest}**/*`]).on('change', browserSync.reload);
 });
 
 gulp.task('default',['sass','html','assets','js','serve']);
